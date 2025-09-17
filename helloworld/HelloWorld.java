f98ad6087d8352c0cb68b1f502af47f842f2f8d4
charliefoster-vgs <charlie.foster@keyfactor.com>
2025-09-17 00:14:47 -0400

    Removal of duplicate directory.

--- helloworld/HelloWorld.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        // This line prints the text "Hello, World!" to the console.
-        System.out.println("Hello, World!");
-    }
-}
