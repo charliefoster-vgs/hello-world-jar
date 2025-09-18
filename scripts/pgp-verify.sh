48f291742f9ae791076f135bab16bfbb3219e9e4
charliefoster-vgs <charlie.foster@keyfactor.com>
2025-09-18 13:28:45 -0400

    Added PGP Public Key import.

--- scripts/pgp-verify.sh
+++ scripts/pgp-verify.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-gpg --verify $1 $2
+PGP_PUBLIC_KEY_FILE="./OpenPGPSigner-signer00003.asc"
+
+echo $PGP_PUBLIC_KEY
+
+if [ -f "$PGP_PUBLIC_KEY_FILE" ]; then
+    gpg --import "$PGP_PUBLIC_KEY_FILE"
+    echo "PGP Public key from $PGP_KEY_FILE imported successfully."
+
+    gpg --verify $1 $2
+else
+    echo "Error: Key file $PGP_PBULIC_KEY_FILE not found."
+fi
